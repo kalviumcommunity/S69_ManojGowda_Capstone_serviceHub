8bc7c0e72ca23b58208994eea8a55d444ca89e46
ManojGowda2006 <manojgowdan807@gmail.com>
2025-03-20 10:52:52 +0530

    component

--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import logo from '../assets/logo.svg'
+import React from 'react';
+import logo from '../assets/logo.svg';
 
 function NavBar() {
-    return(
-        <div className='flex items-center gap-4'>
-          <img src={logo} className='h-15 w-15 ml-3 mt-4' alt='logo'/>
-          <h4 className='font-[jetbrains mono] font-bold text-black text-[52px]'>serviceHub</h4>
-        </div>
-    )
+  return (
+    <div className="flex items-center justify-between px-6 py-4">
+      {/* Logo & Title */}
+      <div className="flex items-center gap-4">
+        <img draggable="false" src={logo} className="h-12 w-12 " alt="logo" />
+        <h4 className="font-['JetBrains Mono'] font-bold text-white text-2xl sm:text-3xl lg:text-4xl">
+          serviceHub
+        </h4>
+      </div>
+
+      {/* Navigation Buttons */}
+      <div className="flex items-center gap-4">
+        <button className=" text-white text-xl px-4 py-2 rounded-lg font-normal hover:text-gray-300 transition cursor-pointer">
+          Sign Up
+        </button>
+        <button className=" text-white text-xl px-4 py-2 rounded-lg font-normal hover:text-gray-300 transition cursor-pointer">
+          Login
+        </button>
+        <button className=" text-white text-xl px-4 py-2 rounded-lg font-normal hover:text-gray-300 transition cursor-pointer">
+          Professional Register
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
--- client/src/pages/landingPage.jsx
+++ client/src/pages/landingPage.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import NavBar from '../components/navBar'
-import bgImage from '../assets/bg.jpeg'
-import '../App.css'
-// import './LandingPage.css'
+import React from 'react';
+import NavBar from '../components/navBar';
+import bgImage from '../assets/bg.jpeg';
+import '../App.css';
 
-function LandingPage(){
-  return(
-    <div>
+function LandingPage() {
+  return (
+    <div className="relative h-screen w-full">
+      
+      <div 
+        className="absolute inset-0 bg-cover  filter brightness-75 contrast-125"
+        style={{ backgroundImage: `url(${bgImage})` }}
+      />
 
-    {/*  <div style={{ backgroundImage: `url(${bgImage})` }} className="bg-cover h-screen w-full" > */}
-    <NavBar/>
-        <div className='watermark '>
-          <img src={bgImage}/>
-    Arav
+      <div className="relative z-10">
+        <NavBar />
+      </div>
     </div>
- </div>
-  )
+  );
 }
 
 export default LandingPage;
